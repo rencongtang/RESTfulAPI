6c2180a34078b88e8bf4129859ba91adce10e54c
MarioTang Mac <rencongtang@outlook.com>
2019-03-10 15:11:18 +0100

    add the java apache client

---
+++ Client_core/Repo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Client_core
+{ 
+    //now it's time to process the json data at first, we need to make sure
+    //the class name is same as the url element
+    public class Repo
+    {
+        public string name;
+    }
+}
